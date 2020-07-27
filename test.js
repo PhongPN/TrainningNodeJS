cf1f5876f9c0f8c4a535ab5056d9705d53ab229c
PhongPN <Phongphamngoc123@gmail.com>
2020-07-27 11:28:17 +0700

    install fsextra

---
+++ test.js
@@ -0,0 +1,28 @@
+// s
+
+//Change array
+// let actor = [
+//     { firstName: "Robert", lastName: "Downey .JR" },
+//     {firstName: "Json", lastName: "Statham" }
+// ]
+// let  [a,b] = actor;
+
+// let actorFullName = [
+//     {fullname: a.firstName + a.lastName},
+//     {fullname: b.firstName + b.lastName}
+// ]
+// let [rd,js] = actorFullName;
+
+// console.log ("Name:" + rd.fullname);
+// // console.log ("Name:" + js.fullname);
+
+//count number
+// console.log (actorFullName.length);
+
+//Sum number
+var constNumbers = [1,2,3,4,5]
+var sum = 0
+for(let i=0; i<constNumbers.length;i++){
+    sum += constNumbers[i];
+}
+console.log (sum);
