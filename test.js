a2ae94242c985f755d2632261a1d3226f206c561
PhongPN <Phongphamngoc123@gmail.com>
2020-07-27 11:43:37 +0700

    update

--- test.js
+++ test.js
@@ -1,23 +1,27 @@
-// s
+// Reverse this string "lairotuT SJedoN"
+var reverseString = str =>{
+    return str.split("").reverse().join("")
+};
+reverseString("lairotuT SJedoN")
 
 //Change array
-// let actor = [
-//     { firstName: "Robert", lastName: "Downey .JR" },
-//     {firstName: "Json", lastName: "Statham" }
-// ]
-// let  [a,b] = actor;
+let actor = [
+    { firstName: "Robert", lastName: "Downey .JR" },
+    {firstName: "Json", lastName: "Statham" }
+]
+let  [a,b] = actor;
 
-// let actorFullName = [
-//     {fullname: a.firstName + a.lastName},
-//     {fullname: b.firstName + b.lastName}
-// ]
-// let [rd,js] = actorFullName;
+let actorFullName = [
+    {fullname: a.firstName + a.lastName},
+    {fullname: b.firstName + b.lastName}
+]
+let [rd,js] = actorFullName;
 
-// console.log ("Name:" + rd.fullname);
-// // console.log ("Name:" + js.fullname);
+console.log ("Name:" + rd.fullname);
+console.log ("Name:" + js.fullname);
 
 //count number
-// console.log (actorFullName.length);
+console.log (actorFullName.length);
 
 //Sum number
 var constNumbers = [1,2,3,4,5]
